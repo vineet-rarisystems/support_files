d36d375a30a01e6cf9838c25ad0e4d0e2356abdc
vineet-rarisystems <vineet@rarisystems.com>
2020-12-08 17:51:36 +0530

    add structure

--- ngsetup.py
+++ ngsetup.py
@@ -1,11 +1,13 @@
 import socket
 import sctp
+#from pycrate_crypto.EAP import *
 from pycrate_asn1dir.NGAP import *
 from pycrate_mobile.NAS             import *
-from pycrate_core.elt               import _with_json
-from pycrate_mobile.TS24501_FGMM import *
-from pycrate_mobile.TS24501_FGSM import *
+#from pycrate_core.elt               import _with_json
+#from pycrate_mobile.TS24501_FGMM import *
+#from pycrate_mobile.TS24501_FGSM import *
 
+#TODO: comments missing
 sk = sctp.sctpsocket_tcp(socket.AF_INET)
 sk.connect(("127.0.0.1",38412))
 
@@ -13,6 +15,9 @@ print("creating ngsetup request Message")
 
 PDU = NGAP_PDU_Descriptions.NGAP_PDU
 
+
+print("show PDU     ------------")
+show(PDU)
 #struct = {'procedureCode': 21, 'criticality': 'reject', 'value': ('NGAPSetupRequest', {'protocolIEs': [{'id': , 'criticality': 'reject', 'value': ('AMF-UE-NGAP-ID', 1)}, {'id': 85, 'criticality': 'reject', 'value': ('RAN-UE-NGAP-ID', 0)}, {'id': 64, 'criticality': 'reject', 'value': ('PDUSessionResourceModifyListModReq', [{'pDUSessionID': 1, 'nAS-PDU': b'~\x02\x00\x00\x00\x00\x04~\x00h\x01\x00\x04.\x01\x01\xcb\x12\x01', 'pDUSessionResourceModifyRequestTransfer': ('PDUSessionResourceModifyRequestTransfer', {'protocolIEs': [{'id': 140, 'criticality': 'reject', 'value': ('UL-NGU-UP-TNLModifyList', [{'uL-NGU-UP-TNLInformation': ('gTPTunnel', {'transportLayerAddress': (32895, 16), 'gTP-TEID': b'\x00\x00\x02\xf0'}), 'dL-NGU-UP-TNLInformation': ('gTPTunnel', {'transportLayerAddress': (0, 1), 'gTP-TEID': b'\x01\xf0\x7f\x00'})}])}]})}])}]})}
 #PDU.set_val(struct)
 #PDU = NGAP.NGAP_PDU_Descriptions.NGAP_PDU
@@ -20,8 +25,6 @@ PDU = NGAP_PDU_Descriptions.NGAP_PDU
 #gnbId = "\x00\x01\x02"
 #gnbID bit length = 24 
 #RANNodeName = "free5gc"
-
-
 #PDU.from_aper(unhexlify("00150035000004001b00080002f83910000102005240090300667265653567630066001000000000010002f839000010080102030015400140"))
 
 IEs = []
